00f5c1a65c78988694f9c00e93a2f6f684bd897f
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-02-08 15:40:21 +0300

    Added tabs section

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,10 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageState extends State<Homepage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
@@ -66,6 +67,40 @@ class _HomepageState extends State<Homepage> {
                     fontSize: 30,
                     fontWeight: FontWeight.w900),
               ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 30, bottom: 30, right: 30),
+            child: Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                    controller: _tabController,
+                    unselectedLabelColor: Color(0xffA4A4A4),
+                    labelColor: Color(0xffffffff),
+                    labelPadding: EdgeInsets.only(left: 30, right: 30),
+                    isScrollable: true,
+                    indicator: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color(0xff5F4D3C)),
+                    tabs: [
+                      Tab(text: 'Catagory1'),
+                      Tab(text: 'Category2'),
+                      Tab(text: 'Category3')
+                    ]),
+              ),
+            ),
+          ),
+          Container(
+            height: 300,
+            width: screenWidth,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: TabBarView(controller: _tabController, children: [
+                Text('Catagory1'),
+                Text("Category2"),
+                Text("category3")
+              ]),
             ),
           )
         ],
